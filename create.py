9db3e9beb1be76a7dbd6164d07aed60f092349e4
Gideon <87426140+GideonBear@users.noreply.github.com>
2022-12-01 10:54:46 +0100

    Update create.py

--- create.py
+++ create.py
@@ -1,3 +1,4 @@
+import sys
 import re
 from pathlib import Path
 
@@ -12,11 +13,18 @@ with open('{}.txt') as f:
 
 
 here = Path()
-years = (year for year in here.iterdir() if num.match(year.name))
-new_yearnum = max(int(year.name) for year in years) + 1
+
+if len(sys.argv) > 1:
+    new_yearnum = int(sys.argv[1])
+else:
+    years = (year for year in here.iterdir() if num.match(year.name))
+    new_yearnum = max(int(year.name) for year in years) + 1
+
 new_year = here / str(new_yearnum)
 new_year.mkdir()
 
 for i in range(1, 26):
     (new_year / f'{i}a.py').write_text(text.format(i))
     (new_year / f'{i}b.py').write_text(text.format(i))
+
+print(f'Created {new_year}')
