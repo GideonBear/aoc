ce328c5e215b8240c60bb3590e79fed3063ad56c
Gideon <87426140+GideonBear@users.noreply.github.com>
2022-12-02 09:59:45 +0100

    22/2

--- 22/2a.py
+++ 22/2a.py
@@ -1,5 +1,63 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class Choice(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+    @classmethod
+    def from_letter(cls, letter: str) -> Choice:
+        return LETTER_MAP[letter]
+
+    def winscore(self, opponent: Choice) -> int:
+        if opponent == self:
+            return DRAW
+        elif BEAT_MAP[self] == opponent:
+            return WIN
+        else:
+            return LOSS
+
+
+Round = tuple[Choice, Choice]
+
+
+LETTER_MAP = {
+    'A': Choice.ROCK,
+    'B': Choice.PAPER,
+    'C': Choice.SCISSORS,
+    'X': Choice.ROCK,
+    'Y': Choice.PAPER,
+    'Z': Choice.SCISSORS,
+}
+BEAT_MAP = {
+    Choice.ROCK: Choice.SCISSORS,
+    Choice.PAPER: Choice.ROCK,
+    Choice.SCISSORS: Choice.PAPER,
+}
+WIN = 6
+DRAW = 3
+LOSS = 0
+
+
+def score_from_round(round: Round) -> int:
+    opponent, you = round
+    choicescore = you.value
+    winscore = you.winscore(opponent)
+    return choicescore + winscore
+
+
 with open('2.txt') as f:
     text = f.read().strip('\n')
 
 
+rounds = (
+    (Choice.from_letter(choice)
+     for choice in round.split(' ')
+     ) for round in text.split('\n')
+)
 
+total = sum(score_from_round(round) for round in rounds)
+print(total)
--- 22/2b.py
+++ 22/2b.py
@@ -1,5 +1,78 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class Choice(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+    @classmethod
+    def from_letter(cls, letter: str) -> Choice:
+        return CHOICE_LETTER_MAP[letter]
+
+    @classmethod
+    def from_opponent_endstate(cls, opponent: Choice, endstate: End) -> Choice:
+        if endstate == End.DRAW:
+            return opponent
+        elif endstate == End.LOSS:
+            return BEAT_MAP[opponent]
+        else:
+            return LOSS_MAP[opponent]
+
+
+Round = tuple[Choice, Choice]
+
+
+CHOICE_LETTER_MAP = {
+    'A': Choice.ROCK,
+    'B': Choice.PAPER,
+    'C': Choice.SCISSORS,
+}
+BEAT_MAP = {
+    Choice.ROCK: Choice.SCISSORS,
+    Choice.PAPER: Choice.ROCK,
+    Choice.SCISSORS: Choice.PAPER,
+}
+LOSS_MAP = {
+    v: k for k, v in BEAT_MAP.items()
+}
+
+
+class End(Enum):
+    WIN = 6
+    DRAW = 3
+    LOSS = 0
+
+    @classmethod
+    def from_letter(cls, letter: str) -> End:
+        return END_LETTER_MAP[letter]
+
+
+END_LETTER_MAP = {
+    'X': End.LOSS,
+    'Y': End.DRAW,
+    'Z': End.WIN,
+}
+
+
+def score_from_round(round: Round) -> int:
+    opponent, end = round
+    you = Choice.from_opponent_endstate(opponent, end)
+    choicescore = you.value
+    winscore = end.value
+    return choicescore + winscore
+
+
 with open('2.txt') as f:
     text = f.read().strip('\n')
 
 
+rounds = (
+    (Choice.from_letter(round[0]), End.from_letter(round[2]))
+    for round in text.split('\n')
+)
 
+total = sum(score_from_round(round) for round in rounds)
+print(total)
