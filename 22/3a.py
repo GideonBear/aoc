3a28def711021d7ad5ad45a41852b22f441640d1
GideonBear <87426140+GideonBear@users.noreply.github.com>
2022-12-03 10:02:54 +0100

    22/3

--- 22/3a.py
+++ 22/3a.py
@@ -1,5 +1,14 @@
+from string import ascii_letters
+
+
+priorities = ' ' + ascii_letters
+
+
 with open('3.txt') as f:
     text = f.read().strip('\n')
 
 
-
+rucksacks = ((set(rucksack[:(middle := len(rucksack) // 2)]), set(rucksack[middle:])) for rucksack in text.split('\n'))
+els = (a.intersection(b).pop() for a, b in rucksacks)
+pris = (priorities.index(el) for el in els)
+print(sum(pris))
--- 22/3b.py
+++ 22/3b.py
@@ -1,5 +1,18 @@
+from string import ascii_letters
+
+
+priorities = ' ' + ascii_letters
+
+
+def chunks(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
 with open('3.txt') as f:
     text = f.read().strip('\n')
 
 
-
+groups = chunks([set(elf) for elf in text.split('\n')], 3)
+els = (a.intersection(b).intersection(c).pop() for a, b, c in groups)
+pris = (priorities.index(el) for el in els)
+print(sum(pris))
