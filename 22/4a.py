05b5f62734508a82d70307ba76fc9e4fa432805d
GideonBear <87426140+GideonBear@users.noreply.github.com>
2022-12-04 11:55:35 +0100

    22/4

--- 22/4a.py
+++ 22/4a.py
@@ -1,5 +1,27 @@
+def range_subset(a: range, b: range) -> bool:
+    return a[0] in b and a[-1] in b
+
+
 with open('4.txt') as f:
     text = f.read().strip('\n')
 
 
-
+assignments = (
+    (
+        range(
+            (
+                assignment_range :=
+                [int(num) for num in assignment.split('-')]
+            )[0],
+            assignment_range[1] + 1
+        ) for assignment
+        in pair.split(',')
+    ) for pair
+    in text.split('\n')
+)
+subsets = (
+    range_subset(a, b) or range_subset(b, a)
+    for a, b
+    in assignments
+)
+print(sum(subsets))
--- 22/4b.py
+++ 22/4b.py
@@ -1,5 +1,27 @@
+def range_overlaps(a: range, b: range) -> bool:
+    return bool(range(max(a[0], b[0]), min(a[-1], b[-1])+1))
+
+
 with open('4.txt') as f:
     text = f.read().strip('\n')
 
 
-
+assignments = (
+    (
+        range(
+            (
+                assignment_range :=
+                [int(num) for num in assignment.split('-')]
+            )[0],
+            assignment_range[1] + 1
+        ) for assignment
+        in pair.split(',')
+    ) for pair
+    in text.split('\n')
+)
+overlaps = (
+    range_overlaps(a, b) or range_overlaps(b, a)
+    for a, b
+    in assignments
+)
+print(sum(overlaps))
