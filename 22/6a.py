e5a4bcc1dbbd5ebdff86abb0a717597f34e9cd90
Gideon <87426140+GideonBear@users.noreply.github.com>
2022-12-06 09:29:45 +0100

    22/6

--- 22/6a.py
+++ 22/6a.py
@@ -1,5 +1,11 @@
+from more_itertools import windowed
+
+
 with open('6.txt') as f:
     text = f.read().strip('\n')
 
 
-
+for i, window in enumerate(windowed(text, 4), start=4):
+    if len(window) == len(set(window)):
+        print(i)
+        break
--- 22/6b.py
+++ 22/6b.py
@@ -1,5 +1,11 @@
+from more_itertools import windowed
+
+
 with open('6.txt') as f:
     text = f.read().strip('\n')
 
 
-
+for i, window in enumerate(windowed(text, 14), start=14):
+    if len(window) == len(set(window)):
+        print(i)
+        break
