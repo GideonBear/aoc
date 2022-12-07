a42deac9b97465ab52a4c4434ab9fc1cff11d281
Gideon <87426140+GideonBear@users.noreply.github.com>
2022-12-07 09:28:49 +0100

    22/7

--- 22/7a.py
+++ 22/7a.py
@@ -1,5 +1,57 @@
+class Dir:
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.children: dict[str, int] = {}
+
+    @property
+    def size(self) -> int:
+        return sum(
+            child
+            if isinstance(child, int)
+            else child.size
+            for child
+            in self.children.values())
+
+    def add_dir(self, name: str):
+        if name in self.children:
+            raise ValueError('Child already exists')
+        dir = Dir(self)
+        self.add(name, dir)
+        return dir
+
+    def add(self, name: str, node):
+        self.children[name] = node
+
+
 with open('7.txt') as f:
     text = f.read().strip('\n')
 
 
+root = Dir()
+curr = None
+dirs = []
 
+for line in text.split('\n'):
+    if line == '$ ls':
+        continue
+    elif line == '$ cd /':
+        curr = root
+    elif line == '$ cd ..':
+        curr = curr.parent
+    elif line.startswith('$ cd '):
+        name = line[5:]
+        curr = curr.children[name]
+    elif line.startswith('dir '):
+        name = line[4:]
+        dir = curr.add_dir(name)
+        dirs.append(dir)
+    else:
+        size, name = line.split(' ')
+        curr.add(name, int(size))
+
+print(sum(
+    dir.size
+    for dir
+    in dirs
+    if dir.size <= 100_000
+))
