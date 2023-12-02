888e452e8d0401ba80a570111676183a4f893f6c
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-02 07:55:18 +0100

    c

--- 23/2a.py
+++ 23/2a.py
@@ -1,5 +1,39 @@
 with open('2.txt') as f:
     text = f.read().strip('\n')
 
+cubes = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
 
 
+res = 0
+lines = text.split("\n")
+
+
+def flatten(l):
+    for i in l:
+        yield from i
+
+
+for line in lines:
+    line = line.removeprefix("Game ")
+    gid = ""
+    while line[0].isdigit():
+        gid += line[0]
+        line = line[1:]
+    gid = int(gid)
+    line = line.removeprefix(": ")
+
+    turns = [turn.split(", ") for turn in line.split("; ")]
+    for color_hand in flatten(turns):
+        n, c = color_hand.split(" ")
+        n = int(n)
+
+        if n > cubes[c]:
+            break
+    else:
+        res += gid
+
+print(res)
--- 23/2b.py
+++ 23/2b.py
@@ -1,5 +1,40 @@
+import operator
+from functools import reduce
+
 with open('2.txt') as f:
     text = f.read().strip('\n')
 
 
+res = 0
+lines = text.split("\n")
 
+
+def flatten(l):
+    for i in l:
+        yield from i
+
+
+for line in lines:
+    line = line.removeprefix("Game ")
+    gid = ""
+    while line[0].isdigit():
+        gid += line[0]
+        line = line[1:]
+    gid = int(gid)
+    line = line.removeprefix(": ")
+
+    turns = [turn.split(", ") for turn in line.split("; ")]
+    max_cubes = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    for turn in turns:
+        for color_hand in turn:
+            n, c = color_hand.split(" ")
+            n = int(n)
+            max_cubes[c] = max(n, max_cubes[c])
+    power = reduce(operator.mul, max_cubes.values())
+    res += power
+
+print(res)
