f7a21724b4a979bae3ce61cfbd5b2a051920fd89
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-03 07:41:24 +0100

    c

--- 23/3a.py
+++ 23/3a.py
@@ -1,5 +1,50 @@
+import contextlib
+
 with open('3.txt') as f:
     text = f.read().strip('\n')
 
 
+def is_symbol(s):
+    return not s.isdigit() and s != "."
 
+
+def get_adj(space: tuple[int, int]):
+    y, x = space
+    for vy, vx in ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)):
+        yield y + vy, x + vx
+
+
+lines = text.split("\n")
+nums = []
+for y, line in enumerate(lines):
+    curr = ""
+    curr_spaces = []
+    for x, char in enumerate(line):
+        if char.isdigit():
+            curr += char
+            curr_spaces.append((y, x))
+        else:
+            if curr:
+                nums.append((int(curr), curr_spaces))
+                curr = ""
+                curr_spaces = []
+
+    if curr:
+        nums.append((int(curr), curr_spaces))
+
+res = 0
+for num, spaces in nums:
+    num_done = False
+    for space in spaces:
+        for y, x in get_adj(space):
+            with contextlib.suppress(IndexError):
+                if is_symbol(lines[y][x]):
+                    #print(f"Found {num} : {space} to be adjacent, adding to res")
+                    res += num
+                    num_done = True
+                    break
+        if num_done:
+            #print(f"Found {num} : {space} to be non-adjacent, trying next space")
+            break
+
+print(res)
--- 23/3b.py
+++ 23/3b.py
@@ -1,5 +1,63 @@
+import contextlib
+import operator
+from functools import reduce
+
 with open('3.txt') as f:
     text = f.read().strip('\n')
 
 
+def is_symbol(s):
+    return not s.isdigit() and s != "."
 
+
+def get_adj(space: tuple[int, int]):
+    y, x = space
+    for vy, vx in ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)):
+        yield y + vy, x + vx
+
+
+lines = text.split("\n")
+
+nums = []
+for y, line in enumerate(lines):
+    curr = ""
+    curr_spaces = []
+    for x, char in enumerate(line):
+        if char.isdigit():
+            curr += char
+            curr_spaces.append((y, x))
+        else:
+            if curr:
+                nums.append((int(curr), curr_spaces))
+                curr = ""
+                curr_spaces = []
+
+    if curr:
+        nums.append((int(curr), curr_spaces))
+
+stars = []
+for y, line in enumerate(lines):
+    for x, char in enumerate(line):
+        if char == "*":
+            stars.append((y, x))
+
+res = 0
+for star in stars:
+    #print(f"Trying star {star}")
+    adj_nums = []
+    done_nums = []
+    for adj in get_adj(star):
+        for num, num_spaces in nums:
+            if num in done_nums:
+                continue
+            for num_space in num_spaces:
+                if num_space == adj:
+                    #print(f"Adj space {adj} found to be occupied by {num}")
+                    adj_nums.append(num)
+                    done_nums.append(num)
+    if len(adj_nums) == 2:
+        res += reduce(operator.mul, adj_nums)
+    elif len(adj_nums) == 3:
+        print("- 3 adj_nums, nothing done")
+
+print(res)
