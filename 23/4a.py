b1931b5125e6e859a9e7a135c9da93484a96e4e6
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-04 16:58:54 +0100

    c

--- 23/4a.py
+++ 23/4a.py
@@ -1,5 +1,20 @@
+from pprint import pprint
+
 with open('4.txt') as f:
     text = f.read().strip('\n')
 
 
+cards = text.split("\n")
+cards = (card[card.find(":") + 2:] for card in cards)
+cards = (card.split(" | ") for card in cards)
+cards = list(tuple(map(str.split, card)) for card in cards)
+assert all(len(card) == 2 for card in cards)
 
+total = 0
+for win_nums, my_nums in cards:
+    num_matching_nums = len(list(filter(lambda my_num: my_num in win_nums, my_nums)))
+    if num_matching_nums == 0:
+        continue
+    points = 2 ** (num_matching_nums - 1)
+    total += points
+print(total)
--- 23/4b.py
+++ 23/4b.py
@@ -1,5 +1,26 @@
+import itertools
+from pprint import pprint
+
 with open('4.txt') as f:
     text = f.read().strip('\n')
 
 
+cards = text.split("\n")
+cards = (card[card.find(":") + 2:] for card in cards)
+cards = (card.split(" | ") for card in cards)
+cards = dict(zip(
+    itertools.count(),
+    ((i,) + tuple(map(str.split, card)) for i, card in enumerate(cards))
+))
 
+total = 0
+q = list(cards.values())
+while q:
+    print(f"{len(q)=}")
+    total += 1
+    i, win_nums, my_nums = q.pop()
+    num_matching_nums = len(list(filter(lambda my_num: my_num in win_nums, my_nums)))
+    #print(f"Found {num_matching_nums} matches in card {i}")
+    for j in range(1, num_matching_nums + 1):
+        q.append(cards[i + j])
+print(total)
