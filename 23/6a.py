5b4988241578d56ab5f0d6325a8482da2f0d7886
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-06 17:21:35 +0100

    c

--- 23/6a.py
+++ 23/6a.py
@@ -2,4 +2,18 @@ with open('6.txt') as f:
     text = f.read().strip('\n')
 
 
+times, record_distances = (map(int, line.split()[1:]) for line in text.split("\n"))
+races = [(time, record_distance) for time, record_distance in zip(times, record_distances)]
 
+total = 1
+for time, record_distance in races:
+    ways_to_beat = 0
+    for holding_time in range(time + 1):
+        speed = holding_time
+        time_left = time - holding_time
+        distance = time_left * speed
+        if distance > record_distance:
+            ways_to_beat += 1
+    total *= ways_to_beat
+
+print(total)
--- 23/6b.py
+++ 23/6b.py
@@ -1,5 +1,21 @@
+from tqdm import tqdm
+
 with open('6.txt') as f:
     text = f.read().strip('\n')
 
 
+times, record_distances = (int("".join(line.split()[1:])) for line in text.split("\n"))
+races = [(times, record_distances)]
 
+total = 1
+for time, record_distance in races:
+    ways_to_beat = 0
+    for holding_time in tqdm(range(time + 1)):
+        speed = holding_time
+        time_left = time - holding_time
+        distance = time_left * speed
+        if distance > record_distance:
+            ways_to_beat += 1
+    total *= ways_to_beat
+
+print(total)
