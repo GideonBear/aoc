fa8cdb6723923acefe317da2bf0fc563f193a7d3
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-07 19:28:33 +0100

    c

--- 23/5b.rs
+++ 23/5b.rs
@@ -5,6 +5,7 @@
 
 use std::fs;
 use itertools::Itertools;
+use std::ops::Range;
 
 struct Ma {
     from_cat: String,
@@ -28,10 +29,34 @@ impl Ma {
                 .collect(),
         }
     }
+
+    fn get(&self, item: u16) -> u16 {
+        for (dest_start, source_start, length) in self.ranges {
+            if source_start <= item && item <= source_start + length {
+                return dest_start + (item - source_start);
+            }
+        }
+        item
+    }
 }
 
 fn main() {
     let text = fs::read_to_string("5.txt").expect("Error while reading file");
 
-
+    let groups = text.split("\n\n");
+    let seeds: Vec<(u16, u16)> = groups
+        .next().unwrap()
+        .strip_prefix("seeds: ").unwrap()
+        .split(' ')
+        .map(|x| x.parse())
+        .chunks(2)
+        .into_iter()
+        .map(|x| x.collect_tuple().unwrap())
+        .collect();
+    let total_seeds = seeds.iter().map(|(start, length)| length).sum();
+    let seeds = seeds
+        .into_iter()
+        .map(|(start, length)| Range { start, end: start + length })
+        .flatten();
+    let old_mas = groups;
 }
