c09e6323999472afc89617dca8ce26fe0c2b0b60
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-10 09:06:35 +0100

    c

--- 23/10a.rs
+++ 23/10a.rs
@@ -10,6 +10,7 @@ use std::fs;
 use grid::*;
 use itertools::Itertools;
 
+#[derive(PartialEq, Eq, Debug)]
 enum PipeVector {
     Start,
     Ground,
@@ -37,8 +38,19 @@ impl PipeVector {
     }
 }
 
-fn grid_vec(grid: &Grid, vec: &(isize, isize)) -> Option<(usize, usize)> {
-    todo!();
+fn grid_vec(grid: &Grid<usize>, row: usize, col: usize, vec: &(isize, isize)) -> Option<(usize, usize)> {
+    let new_row = isize::try_from(row).unwrap() + vec.0;
+    let new_col = isize::try_from(col).unwrap() + vec.1;
+    let (grid_height, grid_width) = grid.size();
+    if new_row < 0 || new_col < 0 ||
+        new_row > grid_height.try_into().unwrap() || new_col > grid_width.try_into().unwrap() {
+        return None;
+    }
+    Some((new_row.try_into().unwrap(), new_col.try_into().unwrap()))
+}
+
+fn neg_vec(vec: &(isize, isize)) -> (isize, isize) {
+    (-vec.0, -vec.1)
 }
 
 fn main() {
@@ -49,6 +61,34 @@ fn main() {
     let mut grid = Grid::from_vec(
         lines.join("").chars().map(PipeVector::new).collect(), width
     );
+
+    let (start, _) = grid.indexed_iter().find(|((_row, _col), vec)| vec == &&PipeVector::Start).unwrap();
+    println!("start at {start:?}");
+
+    let mut in_loop = vec![];
+    let curr = start;
+    let last_vec = (0, 0);
+    while !in_loop.contains(&curr) {
+        in_loop.push(curr);
+        match grid[curr] {
+            PipeVector::Start => {
+                for vec in [NORTH, EAST, SOUTH, WEST] {
+                    todo!();
+                }
+            }
+            PipeVector::Vectors(vec1, vec2) => {
+                let vec;
+                if vec1 == neg_vec(&last_vec) {
+                    vec = vec2;
+                } else if vec2 == neg_vec(&last_vec) {
+                    vec = vec1;
+                } else {
+                    panic!();
+                }
+            }
+            PipeVector::Ground => unreachable!(),
+        }
+    }
 
     todo!();
 }
