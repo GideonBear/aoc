ee09f507ed420a69eb0c063dca12b7d9bbf542f2
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-11 11:05:33 +0100

    c

--- 23/10a.rs
+++ 23/10a.rs
@@ -9,6 +9,7 @@
 use std::fs;
 use grid::*;
 use itertools::Itertools;
+use std::collections::HashMap;
 
 #[derive(PartialEq, Eq, Debug)]
 enum PipeVector {
@@ -17,8 +18,8 @@ enum PipeVector {
     Vectors((isize, isize), (isize, isize)),
 }
 
-const NORTH: (isize, isize) = (1, 0);
-const SOUTH: (isize, isize) = (-1, 0);
+const NORTH: (isize, isize) = (-1, 0);
+const SOUTH: (isize, isize) = (1, 0);
 const EAST: (isize, isize) = (0, 1);
 const WEST: (isize, isize) = (0, -1);
 
@@ -38,7 +39,8 @@ impl PipeVector {
     }
 }
 
-fn grid_vec(grid: &Grid<usize>, row: usize, col: usize, vec: &(isize, isize)) -> Option<(usize, usize)> {
+fn grid_vec<T>(grid: &Grid<T>, coords: &(usize, usize), vec: &(isize, isize)) -> Option<(usize, usize)> {
+    let &(row, col) = coords;
     let new_row = isize::try_from(row).unwrap() + vec.0;
     let new_col = isize::try_from(col).unwrap() + vec.1;
     let (grid_height, grid_width) = grid.size();
@@ -54,26 +56,66 @@ fn neg_vec(vec: &(isize, isize)) -> (isize, isize) {
 }
 
 fn main() {
-    let text = fs::read_to_string("10e1.txt").expect("Error while reading file");
+    let text = fs::read_to_string("10.txt").expect("Error while reading file");
 
     let mut lines = text.split('\n').peekable();
     let width = lines.peek().unwrap().len();
-    let mut grid = Grid::from_vec(
+    let grid = Grid::from_vec(
         lines.join("").chars().map(PipeVector::new).collect(), width
     );
 
     let (start, _) = grid.indexed_iter().find(|((_row, _col), vec)| vec == &&PipeVector::Start).unwrap();
     println!("start at {start:?}");
 
-    let mut in_loop = vec![];
-    let curr = start;
-    let last_vec = (0, 0);
-    while !in_loop.contains(&curr) {
-        in_loop.push(curr);
+    let mut in_loop: HashMap<(usize, usize), u32> = HashMap::new();
+    let mut curr = start;
+    let mut last_vec = (0, 0);
+    let mut starting_vecs_done = vec![];
+    let mut dist = 0;
+    loop {
+        in_loop.entry(curr).and_modify(|old_dist| *old_dist = dist.min(*old_dist)).or_insert(dist);
+        if in_loop.contains_key(&curr) {
+            //println!("Replacing {} with {}, dist={dist}", in_loop[&curr], dist.min(in_loop[&curr]));
+            in_loop.insert(curr, dist.min(in_loop[&curr]));
+        } else {
+            in_loop.insert(curr, dist);
+        }
+        //dbg!(&in_loop);
         match grid[curr] {
             PipeVector::Start => {
+                let mut done = true;
+                dist = 0;
                 for vec in [NORTH, EAST, SOUTH, WEST] {
-                    todo!();
+                    if starting_vecs_done.contains(&vec) {
+                        //println!("Skipping {vec:?}");
+                        continue;
+                    }
+                    match grid_vec(&grid, &curr, &vec) {
+                        Some(coords) => {
+                            println!("Found possible starting point at {coords:?}");
+                            starting_vecs_done.push(vec);
+                            match grid[coords] {
+                                PipeVector::Vectors(vec1, vec2) => {
+                                    if vec1 == neg_vec(&vec) || vec2 == neg_vec(&vec) {
+                                        println!("Was correct starting point, storing curr, last_vec, incrementing dist, and exiting");
+                                        curr = coords;
+                                        last_vec = vec;
+                                        dist += 1;
+                                        done = false;
+                                        break;
+                                    }
+                                    println!("Neither vec ({vec1:?}, {vec2:?}) matching neg vec ({vec:?}), trying next");
+                                }
+                                PipeVector::Ground => (),
+                                PipeVector::Start => panic!(),
+                            }
+                        }
+                        None => (),
+                    }
+                }
+                if done {
+                    println!("Done; breaking");
+                    break;
                 }
             }
             PipeVector::Vectors(vec1, vec2) => {
@@ -83,12 +125,24 @@ fn main() {
                 } else if vec2 == neg_vec(&last_vec) {
                     vec = vec1;
                 } else {
-                    panic!();
+                    panic!("Neither vec ({vec1:?}, {vec2:?}) matching neg last_vec ({last_vec:?})");
+                }
+                //println!("vec = {vec:?}");
+                match grid_vec(&grid, &curr, &vec) {
+                    None => panic!("Vector {vec:?} invalid"),
+                    Some(coords) => {
+                        println!("Found next at {coords:?}, storing curr, last_vec, incrementing dist");
+                        curr = coords;
+                        last_vec = vec;
+                        dist += 1;
+                    },
                 }
             }
             PipeVector::Ground => unreachable!(),
         }
     }
 
-    todo!();
+    println!("{in_loop:?}");
+    let answer = in_loop.into_values().max();
+    println!("{answer:?}");
 }
