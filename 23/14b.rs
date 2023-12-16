ebc39cdcf50a7722c9b0448377fc0023bbddc8fe
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-16 14:45:17 +0100

    c

--- 23/14b.rs
+++ 23/14b.rs
@@ -63,9 +63,9 @@ fn tilt(grid: &mut Grid<Space>, dir: (isize, isize)) {
 
 fn spin_cycle(grid: &mut Grid<Space>) {
     tilt(grid, NORTH);
-    tilt(grid, EAST);
-    tilt(grid, SOUTH);
     tilt(grid, WEST);
+    tilt(grid, SOUTH);
+    tilt(grid, EAST);
 }
 
 fn get_load(grid: &mut Grid<Space>, dir: (isize, isize)) -> usize {
@@ -80,7 +80,7 @@ fn get_load(grid: &mut Grid<Space>, dir: (isize, isize)) -> usize {
     let load = grid
         .indexed_iter()
         .filter(|(_coords, &space)| space == Space::Round)
-        .map(|((row, col), space)| { println!("{row}, {col}, {space:?}"); ((row, col), space) })
+        .map(|((row, col), space)| { /*println!("{row}, {col}, {space:?}");*/ ((row, col), space) })
         .map(|((row, _col), _space)| row + 1)
         .sum();
     do_after(grid);
@@ -107,11 +107,37 @@ fn main() {
         lines.join("").chars().map(Space::from_char).collect(), width
     );
 
+    let total_spins = 1_000_000_000;
     print_grid(&grid);
-    for i in tqdm(0..1_000_000_000) {
+    let mut history = vec![];
+    let mut tracking = None;
+    let mut cycle_length = 0;
+    let mut ending_i = None;
+    for i in tqdm(0..total_spins) {
+        if let Some(ref t) = tracking {
+            cycle_length += 1;
+            if grid == *t {
+                println!("Found tracked snapshot again with cycle length {cycle_length}");
+                ending_i = Some(i);
+                break;
+            }
+        }
+        if tracking.is_none() && history.contains(&grid) {
+            println!("Tracking snapshot");
+            tracking = Some(grid.clone());
+        }
+        history.push(grid.clone());
+        let old_grid = grid.clone();
+        spin_cycle(&mut grid);
+        if old_grid == grid {
+            println!("Nothing changed after spin cycle ({i})");
+        }
+    }
+    let spin_cycles_to_go = total_spins - (ending_i.unwrap());
+    let spin_cycles_to_go_modded = spin_cycles_to_go % cycle_length;
+    for i in 0..spin_cycles_to_go_modded {
         spin_cycle(&mut grid);
     }
-    print_grid(&grid);
     let load = get_load(&mut grid, SOUTH);
     println!("{load}");
 }
