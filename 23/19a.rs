05ef0a90b1ec8face298072155e791de2d730c9d
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-19 15:14:54 +0100

    c

--- 23/19a.rs
+++ 23/19a.rs
@@ -1,5 +1,99 @@
 use std::fs;
 
+enum Cat {
+    X,
+    M,
+    A,
+    S,
+}
+
+impl Cat {
+    fn from_char(c: char) -> Self {
+        match c {
+            'x' => Self::X,
+            'm' => Self::M,
+            'a' => Self::A,
+            's' => Self::S,
+            _ => panic!(),
+        }
+    }
+}
+
+struct Workflow {
+    rules: Vec<(Condition, SendTo)>,
+    fallback: SendTo,
+}
+
+impl Workflow {
+    fn from_string(s: &str) -> Self {
+        todo!()
+    }
+}
+
+struct Condition {
+    cat: Cat,
+    op: Op,
+    val: u32,
+}
+
+impl Condition {
+    fn from_string(s: &str) -> Self {
+        let it = s.chars();
+        let cat = Cat::from_char(it.next());
+        let op = Op::from_char(it.next());
+        let val = it.collect::<String>().parse().unwrap();
+        Self { cat, op, val }
+    }
+}
+
+enum Op {
+    LT,
+    GT,
+}
+
+impl Op {
+    fn from_char(c: char) -> Self {
+        match c {
+            '<' => Self::LT,
+            '>' => Self::GT,
+            _ => panic!(),
+        }
+    }
+
+    fn apply<T>(&self, a: T, b: T) -> bool {
+        match self {
+            Self::LT => a < b,
+            Self::GT => a > b,
+        }
+    }
+}
+
+enum SendTo {
+    Accept,
+    Reject,
+    Workflow(String),
+}
+
+impl SendTo {
+    fn from_string(s: &str) -> Self {
+        match s {
+            "A" => Self::Accept,
+            "R" => Self::Reject,
+            s => Self::Workflow(s),
+        }
+    }
+}
+
+struct Part {
+    ratings: HashMap<Cat, u32>,
+}
+
+impl Part {
+    fn from_string(s: &str) -> Self {
+        todo!()
+    }
+}
+
 fn main() {
     let text = fs::read_to_string("19.txt").expect("Error while reading file");
 
