1e12b78ff7038990c6d9cab907ae0bd4e136ea24
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-12-19 15:02:34 +0100

    c

--- 23/19a.rs
+++ 23/19a.rs
@@ -2,6 +2,8 @@ use std::fs;
 
 fn main() {
     let text = fs::read_to_string("19.txt").expect("Error while reading file");
+
+    let (workflows, parts) = text.split("\n\n");
+    let workflows = workflows.map(Workflow::from_string);
+    let parts = parts.map(Part::from_string);
 }
-
-
