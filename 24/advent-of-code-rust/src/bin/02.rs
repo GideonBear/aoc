e7d2ff24195548755dcd323d70c75f392c8f52a4
GideonBear <87426140+GideonBear@users.noreply.github.com>
2024-12-03 15:48:14 +0100

    c

--- 24/advent-of-code-rust/src/bin/02.rs
+++ 24/advent-of-code-rust/src/bin/02.rs
@@ -1,11 +1,66 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let reports: Vec<Vec<u32>> = input
+        .split("\n")
+        .map(|r| r.split(" "))
+        .map(|r| r.map(|l| l.parse::<u32>().unwrap()).collect())
+        .collect();
+    let mut safe = 0;
+    'outer: for mut report in reports {
+        let first = report.get(0).unwrap();
+        let second = report.get(1).unwrap();
+        let allowed = if second > first {
+            [1, 2, 3]
+        } else if second < first {
+            [-1, -2, -3]
+        } else {
+            continue;
+        };
+        for (l1, l2) in report.windows(2).map(|w| (w[0], w[1])) {
+            if !allowed.contains(&(i32::try_from(l2).unwrap() - i32::try_from(l1).unwrap())) {
+                continue 'outer;
+            }
+        }
+        safe += 1;
+    }
+    Some(safe)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let reports: Vec<Vec<u32>> = input
+        .split("\n")
+        .map(|r| r.split(" "))
+        .map(|r| r.map(|l| l.parse::<u32>().unwrap()).collect())
+        .collect();
+    let mut safe = 0;
+    for mut o_report in reports {
+        'outer: for i in 0..(o_report.len() + 1) {
+            let mut report = o_report.clone();
+            if (i != o_report.len()) {
+                report.remove(i);
+            }
+            let first = report.get(0).unwrap();
+            let second = report.get(1).unwrap();
+            let allowed = if second > first {
+                [1, 2, 3]
+            } else if second < first {
+                [-1, -2, -3]
+            } else {
+                continue;
+            };
+            for (l1, l2) in report.windows(2).map(|w| (w[0], w[1])) {
+                if !allowed.contains(&(i32::try_from(l2).unwrap() - i32::try_from(l1).unwrap())) {
+                    continue 'outer;
+                }
+            }
+            safe += 1;
+            break;
+        }
+    }
+    Some(safe)
 }
 
 #[cfg(test)]
@@ -15,12 +70,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
